2837ee81bc808d57ea5d2b563cd2e99c2bc9b68e
Anders Kalhauge <anders@kalhauge.dk>
2016-10-12 15:34:27 +0200

    Jacoco added

--- src/main/java/dk/cphbusiness/choir/backend/mock/ChoirManagerDummy.java
+++ src/main/java/dk/cphbusiness/choir/backend/mock/ChoirManagerDummy.java
@@ -11,10 +11,15 @@ public class ChoirManagerDummy implements ChoirManager {
   public ChoirManagerDummy() {
     new Member("Anders", "Kalhauge", "anders@kalhauge.dk", "21724411", "tenor");
     new Member("Kurt", "Hansen", "kurt@mail.dk", "12345678", "tenor");
+    new Member("Sonja", "Hansen", "sonja@mail.dk", "12345678", "tenor");
     }
   
   @Override
   public Collection<MemberSummary> listMembers() {
+//    if (Member.list().size() % 2 == 1) { 
+//      System.out.println("Tjuhej"); 
+//      return null; 
+//      }
     return createMemberSummaries(Member.list());
     }
 
