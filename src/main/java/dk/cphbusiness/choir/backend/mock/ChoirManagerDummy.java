8c6bbc11314643b534bb78c8948777381863fbcb
Anders Kalhauge <anders@kalhauge.dk>
2016-10-12 14:10:36 +0200

    Tests added

--- src/main/java/dk/cphbusiness/choir/backend/mock/ChoirManagerDummy.java
+++ src/main/java/dk/cphbusiness/choir/backend/mock/ChoirManagerDummy.java
@@ -4,31 +4,14 @@ import dk.cphbusiness.choir.backend.mock.model.Member;
 import dk.cphbusiness.choir.contract.ChoirManager;
 import java.util.Collection;
 import static dk.cphbusiness.choir.contract.ChoirManager.*;
-import java.util.ArrayList;
+import static dk.cphbusiness.choir.backend.mock.ChoirAssembler.*;
 
 public class ChoirManagerDummy implements ChoirManager {
-
-  public static MemberSummary createMemberSummary(Member member) {
-    return new MemberSummary(
-        member.getId(),
-        member.getFirstName(),
-        member.getLastName(),
-        member.getVoiceName()
-        );
-    }
-  
-  public static Collection<MemberSummary> createMemberSummaries(Collection<Member> members) {
-    Collection<MemberSummary> summaries = new ArrayList<>();
-    for (Member member : members) summaries.add(createMemberSummary(member));
-    return summaries;
-    }
 
   public ChoirManagerDummy() {
     new Member("Anders", "Kalhauge", "anders@kalhauge.dk", "21724411", "tenor");
     new Member("Kurt", "Hansen", "kurt@mail.dk", "12345678", "tenor");
     }
-  
-  
   
   @Override
   public Collection<MemberSummary> listMembers() {
